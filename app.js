4951a2aa8bd10fdbede03dcf101c81187e56b2e5
. <consolcode625@gmail.com>
2026-01-07 13:37:45 +0530

    Add prime number checker

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-////Q1- Solving the Prime Mystery in a Programming Contest
+////Q1- Teaching a Sibling to Tell Even from Odd
 
-//In a programming contest, you get a question: determine whether a given number is prime. You remember a prime number is a number greater than 1 that has no divisors other than 1 and itself. To solve this, you decide to check divisibility from 2 up to the square root of the number. This way, you can quickly find if any divisor exists and decide primality.
+
+//One day, while teaching your little sibling about numbers, you wonder how a computer can tell if a number is even or odd. You recall that even numbers are divisible by 2 without a remainder, and odd numbers are not. To make it clear, you decide to write a simple program that checks the divisibility by 2 to classify any given number.
 
 
 
@@ -23,3 +24,32 @@ evenOdd = (num) => {
 };
 
 evenOdd(4);
+
+
+//Q2 - Solving the Prime Mystery in a Programming Contest
+
+////In a programming contest, you get a question: determine whether a given number is prime. You remember a prime number is a number greater than 1 that has no divisors other than 1 and itself. To solve this, you decide to check divisibility from 2 up to the square root of the number. This way, you can quickly find if any divisor exists and decide primality.
+
+
+
+const prime = (num) => {
+  // Validate input: prime numbers must be greater than 1
+  if (num <= 1) {
+    console.log("Please enter a number greater than 1");
+    return;
+  }
+
+  // Loop from 2 up to the square root of the number
+  for (let i = 2; i * i <= num; i++) {
+    // If divisible by any number other than 1 and itself
+    if (num % i === 0) {
+      console.log("Not Prime");
+      return;
+    }
+  }
+
+  // If no divisors are found
+  console.log("Prime");
+};
+
+prime(4);
